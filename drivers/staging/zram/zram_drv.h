c6bb64c50c3483559d6ca8aeae5f1d0e4872e1c3
Paul Keith <javelinanddart@gmail.com>
2015-03-15 14:46:36 -0500

    Revert "Merge remote-tracking branch 'cm/cm-12.0' into lollipop"
    This reverts commit 4791225c626bad46faedab26ab12a631bfb7333f, reversing
    changes made to 51492c479cb919ab7073247f2a312e41b2d4e602.

--- drivers/staging/zram/zram_drv.h
+++ drivers/staging/zram/zram_drv.h
@@ -2,7 +2,6 @@
  * Compressed RAM block device
  *
  * Copyright (C) 2008, 2009, 2010  Nitin Gupta
- *               2012, 2013 Minchan Kim
  *
  * This code is released using a dual license strategy: BSD/GPL
  * You can choose the licence that better fits your requirements.
@@ -17,9 +16,9 @@
 #define _ZRAM_DRV_H_
 
 #include <linux/spinlock.h>
-#include <linux/zsmalloc.h>
+#include <linux/mutex.h>
 
-#include "zcomp.h"
+#include "../zsmalloc/zsmalloc.h"
 
 /*
  * Some arbitrary value. This is just to catch
@@ -44,6 +43,7 @@ static const size_t max_zpage_size = PAGE_SIZE / 4 * 3;
 /*-- End of configurable params */
 
 #define SECTOR_SHIFT		9
+#define SECTOR_SIZE		(1 << SECTOR_SHIFT)
 #define SECTORS_PER_PAGE_SHIFT	(PAGE_SHIFT - SECTOR_SHIFT)
 #define SECTORS_PER_PAGE	(1 << SECTORS_PER_PAGE_SHIFT)
 #define ZRAM_LOGICAL_BLOCK_SHIFT 12
@@ -51,24 +51,10 @@ static const size_t max_zpage_size = PAGE_SIZE / 4 * 3;
 #define ZRAM_SECTOR_PER_LOGICAL_BLOCK	\
 	(1 << (ZRAM_LOGICAL_BLOCK_SHIFT - SECTOR_SHIFT))
 
-
-/*
- * The lower ZRAM_FLAG_SHIFT bits of table.value is for
- * object size (excluding header), the higher bits is for
- * zram_pageflags.
- *
- * zram is mainly used for memory efficiency so we want to keep memory
- * footprint small so we can squeeze size and flags into a field.
- * The lower ZRAM_FLAG_SHIFT bits is for object size (excluding header),
- * the higher bits is for zram_pageflags.
- */
-#define ZRAM_FLAG_SHIFT 24
-
-/* Flags for zram pages (table[page_no].value) */
+/* Flags for zram pages (table[page_no].flags) */
 enum zram_pageflags {
 	/* Page consists entirely of zeros */
-	ZRAM_ZERO = ZRAM_FLAG_SHIFT + 1,
-	ZRAM_ACCESS,	/* page in now accessed */
+	ZRAM_ZERO,
 
 	__NR_ZRAM_PAGEFLAGS,
 };
@@ -76,13 +62,14 @@ enum zram_pageflags {
 /*-- Data structures */
 
 /* Allocated for each disk page */
-struct zram_table_entry {
+struct table {
 	unsigned long handle;
-	unsigned long value;
-};
+	u16 size;	/* object size (excluding header) */
+	u8 count;	/* object ref count (not yet used) */
+	u8 flags;
+} __aligned(4);
 
 struct zram_stats {
-<<<<<<< HEAD:drivers/staging/zram/zram_drv.h
 	u64 compr_size;		/* compressed size of pages stored */
 	u64 num_reads;		/* failed + successful */
 	u64 num_writes;		/* --do-- */
@@ -104,30 +91,9 @@ struct zram {
 	spinlock_t stat64_lock;	/* protect 64-bit stats */
 	struct rw_semaphore lock; /* protect compression buffers and table
 				   * against concurrent read and writes */
-=======
-	atomic64_t compr_data_size;	/* compressed size of pages stored */
-	atomic64_t num_reads;	/* failed + successful */
-	atomic64_t num_writes;	/* --do-- */
-	atomic64_t failed_reads;	/* can happen when memory is too low */
-	atomic64_t failed_writes;	/* can happen when memory is too low */
-	atomic64_t invalid_io;	/* non-page-aligned I/O requests */
-	atomic64_t notify_free;	/* no. of swap slot free notifications */
-	atomic64_t zero_pages;		/* no. of zero filled pages */
-	atomic64_t pages_stored;	/* no. of pages currently stored */
-};
-
-struct zram_meta {
-	struct zram_table_entry *table;
-	struct zs_pool *mem_pool;
-};
-
-struct zram {
-	struct zram_meta *meta;
->>>>>>> cm/cm-12.0:drivers/block/zram/zram_drv.h
 	struct request_queue *queue;
 	struct gendisk *disk;
-	struct zcomp *comp;
-
+	int init_done;
 	/* Prevent concurrent execution of device init, reset and R/W request */
 	struct rw_semaphore init_lock;
 	/*
@@ -135,13 +101,8 @@ struct zram {
 	 * we can store in a disk.
 	 */
 	u64 disksize;	/* bytes */
-<<<<<<< HEAD:drivers/staging/zram/zram_drv.h
 
-=======
-	int max_comp_streams;
->>>>>>> cm/cm-12.0:drivers/block/zram/zram_drv.h
 	struct zram_stats stats;
-	char compressor[10];
 };
 
 extern struct zram *zram_devices;
