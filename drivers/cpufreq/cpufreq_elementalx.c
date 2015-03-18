bbf58ab309dc581ea663cf71e90c0f6e15bf45ca
Paul Keith <javelinanddart@gmail.com>
2015-03-18 17:10:30 -0500

    Revert "Let ElementalX Compile as a module"
    This reverts commit 74e331171dd937c2259eacc19ef6365d50f5a517.

--- drivers/cpufreq/cpufreq_elementalx.c
+++ drivers/cpufreq/cpufreq_elementalx.c
@@ -53,8 +53,6 @@ static int g_count = 0;
 static unsigned int min_sampling_rate;
 static unsigned int skip_elementalx = 0;
 
-extern int graphics_boost;
-
 #define LATENCY_MULTIPLIER			(1000)
 #define MIN_LATENCY_MULTIPLIER			(100)
 #define TRANSITION_LATENCY_LIMIT		(10 * 1000 * 1000)
